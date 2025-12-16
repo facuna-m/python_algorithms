71a30fe99ba2ec8ac8508968fcfb341a0e6b1bb9
facuna-m <facuna@ing.ucsc.cl>
2025-12-16 16:31:14 -0300

    added frequency_counter.py

---
+++ frequency_counter.py
@@ -0,0 +1,10 @@
+def frequency_counter(string):
+    frequency = {}
+    for char in string:
+        if char in frequency:
+            frequency[char] += 1
+        else:
+            frequency[char] = 1
+    return frequency
+
+print(frequency_counter("banana"))
