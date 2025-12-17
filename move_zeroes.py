8879b7e6be7e3771fa76e3682dc7f0be330aee2b
facuna-m <facuna@ing.ucsc.cl>
2025-12-17 00:50:37 -0300

    created move_zeroes.py

---
+++ move_zeroes.py
@@ -0,0 +1,14 @@
+def move_zeroes(nums):
+    pos = 0
+    
+    for num in nums:
+        if num != 0:
+            nums[pos] = num
+            pos += 1
+    
+    for i in range(pos, len(nums)):
+        nums[i] = 0
+    
+    return nums
+
+print(move_zeroes([0, 1, 0, 3, 12]))
