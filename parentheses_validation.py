a1dd60f7476c72845e94b61db693bfa92f26fcbe
facuna-m <facuna@ing.ucsc.cl>
2025-12-17 00:38:28 -0300

    created parentheses_validation.py

---
+++ parentheses_validation.py
@@ -0,0 +1,20 @@
+def is_valid(s):
+    stack = []
+    #Mapa de cierres: la clave es el cierre
+    mapa = {")" : "(", "]" :"[", "}" : "{"}
+
+    for char in s:
+        if char in mapa:
+            if stack:
+                top_element = stack.pop()
+            else:
+                top_element = "#" #valor basura
+            if mapa[char] != top_element:
+                return False
+        else:
+            stack.append(char)
+    
+    return not stack
+    
+print(is_valid("()[]{}"))
+print(is_valid("(]"))
