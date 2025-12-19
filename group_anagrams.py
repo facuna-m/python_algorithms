ae9306756aa6a8cbc9188a111bb84adf35ee1695
facuna-m <facuna@ing.ucsc.cl>
2025-12-19 20:28:02 -0300

    created group_anagrams.py

---
+++ group_anagrams.py
@@ -0,0 +1,10 @@
+def group_anagrams(words):
+    groups = {}
+    for word in words:
+        key = "".join(sorted(word))
+        if key not in groups:
+            groups[key] = []
+        groups[key].append(word)
+    return list(groups.values())
+
+print(group_anagrams(["roma", "saco", "amor", "cosa", "perro"]))
