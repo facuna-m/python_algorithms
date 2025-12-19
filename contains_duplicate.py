79a31ac0096d046c9fe94356cbc0ad32515ca93c
facuna-m <facuna@ing.ucsc.cl>
2025-12-19 20:18:29 -0300

    created contains_duplicate.py

---
+++ contains_duplicate.py
@@ -0,0 +1,16 @@
+def contains_duplicate(list):
+    no_dup = set(list)
+    if len(no_dup) == len(list):
+        return False
+    else:
+        return True
+    
+print(contains_duplicate([1, 2, 3, 1]))
+print(contains_duplicate([1, 2, 3, 4]))
+
+
+def contains_duplicate_one_line(list):
+    return len(set(list)) != len(list)
+
+print(contains_duplicate([1, 2, 3, 1]))
+print(contains_duplicate([1, 2, 3, 4]))
